ef2b1110965c5420b8393f77a0d4e418f36e6f5b
geriosla99 <geriosla@gmail.com>
2021-06-16 00:03:04 -0500

    favoritos

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,23 @@
+function favoritos() {
+    var nombre = document.getElementsByClassName('nombre')[0].textContent;
+    var raza = document.getElementsByClassName('raza')[0].textContent;
+    var img = document.getElementsByClassName('mascota')[0].src;
 
+    const mascota = {
+        nombre : nombre,
+        raza : raza,
+        img : img
+    }
+    // console.log(mascota);
+    localStorage.setItem('mascotas',JSON.stringify(mascota));
+    //console.log(localStorage);
+    var favoritos = JSON.parse(localStorage.getItem('mascotas'));
+    alert('Tu mascota se a añadido a favoritos');
+}
+
+function mascota() {
+    
+
+    
+}
 
