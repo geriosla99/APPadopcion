1204b0270bf14b6aa6967ddc1a235723ea2367af
geriosla99 <geriosla@gmail.com>
2021-06-20 17:13:43 -0500

    detalles favorito

--- scripts/index.js
+++ scripts/index.js
@@ -3,69 +3,76 @@ var mascotas = {
     "gatos": [
         {
             "id": "1",
-            "nombre": "Matilde",
-            "raza": "British Shorthair",
-            "color": "gris",
-            "edad": "7 meses",
-            "personalidad": "Muy cariñoso con los niños"
+            "nombre":"Matilde",
+            "raza":"British Shorthair",
+            "color":"gris",
+            "edad":"7 meses",
+            "personalidad":"Muy cariñoso con los niños",
+            "img": "./img/matilde.png"
         },
         {
             "id": "2",
-            "nombre": "Pelusa",
-            "raza": "Birmano",
-            "color": "Plateado",
-            "edad": "3 años",
-            "personalidad": "Es inteligente y muy juiciosa"
+            "nombre":"Pelusa",
+            "raza":"Birmano",
+            "color":"Plateado",
+            "edad":"3 años",
+            "personalidad":"Es inteligente y muy juiciosa",
+            "img":"./img/pelusa.png"
         },
         {
             "id": "3",
-            "nombre": "Kity",
-            "raza": "Bombay",
-            "color": "Blanco",
-            "edad": "2años",
-            "personalidad": "Muy cariñoso"
+            "nombre":"Kity",
+            "raza":"Bombay",
+            "color":"Blanco",
+            "edad":"2años",
+            "personalidad":"Muy cariñoso",
+            "img":"./img/kity.png"
         },
-        {
-            "id": "4",
-            "nombre": "Bombon",
-            "raza": "Gato Americano",
-            "color": "Café",
+        {   "id": "4",
+            "nombre":"Bombon",
+            "raza":"Gato Americano",
+            "color":"Café",
             "edad": "6 meses",
-            "personalidad": "Es hiperactivo y muy juguetón"
+            "personalidad":"Es hiperactivo y muy juguetón",
+            "img":"./img/bombon.png"
         }
     ],
-    "perros": [
-        {
+    "perros" : [
+        {   
             "id": "5",
-            "nombre": "Chester",
-            "raza": "Golden Retriver",
-            "color": "dorado",
-            "edad": "5 meses",
-            "personalidad": "Muy cariñoso con los niños"
+            "nombre":"Chester",
+            "raza":"Golden Retriver",
+            "color":"dorado",
+            "edad":"5 meses",
+            "personalidad":"Muy cariñoso con los niños",
+            "img":"./img/chester.png"
         },
         {
-            "id": "6",
-            "nombre": "Candy",
-            "raza": "Snawser",
-            "color": "Plateado",
-            "edad": "3 años",
-            "personalidad": "Es inteligente y muy juiciosa"
+            "id":"6",
+            "nombre":"Candy",
+            "raza":"Snawser",
+            "color":"Plateado",
+            "edad":"3 años",
+            "personalidad":"Es inteligente y muy juiciosa",
+            "img":"./img/candy.png"
         },
-        {
-            "id": "7",
-            "nombre": "Rocky",
-            "raza": "Poodle",
-            "color": "Blanco",
-            "edad": "2años",
-            "personalidad": "Muy cariñoso"
+        {   
+            "id":"7",
+            "nombre":"Rocky",
+            "raza":"Poodle",
+            "color":"Blanco",
+            "edad":"2años",
+            "personalidad":"Muy cariñoso",
+            "img":"./img/rocky.png"
         },
         {
             "id": "8",
-            "nombre": "Nene",
-            "raza": "Mezclado",
-            "color": "Café",
+            "nombre":"Nene",
+            "raza":"Mezclado",
+            "color":"Café",
             "edad": "6 meses",
-            "personalidad": "Es hiperactivo y muy juguetón"
+            "personalidad":"Es hiperactivo y muy juguetón",
+            "img":"./img/nene.png"
         }
     ]
 }
@@ -163,7 +170,7 @@ function mostrarDetallesMascotas(){
     for (i = 0; i < grupoMascotas.length; i++) {
         if(grupoMascotas[i]['id'] == id){
             console.log(grupoMascotas[i])
-            document.getElementById('imagenMascota').setAttribute('src', './img/'+grupoMascotas[i]['id']+'.png');
+            document.getElementById('imagenMascota').setAttribute('src', grupoMascotas[i]['img']);
             document.getElementById('nombreMascota').innerHTML = grupoMascotas[i]["nombre"];
             document.getElementById('razaMascota').innerHTML = grupoMascotas[i]["raza"];
             document.getElementById('edadMascota').innerHTML = grupoMascotas[i]["edad"];
@@ -172,3 +179,49 @@ function mostrarDetallesMascotas(){
     }
 
 }
+
+function añadirFavoritos() {
+    var nombre = document.getElementById('nombreMascota').innerHTML;
+    var raza = document.getElementById('razaMascota').innerHTML;
+    var img = document.getElementById('imagenMascota').src;
+
+    const mascota = {
+        nombre : nombre,
+        raza : raza,
+        img : img
+    }
+    // console.log(mascota);
+    localStorage.setItem('mascotas',JSON.stringify(mascota));
+    //console.log(localStorage);
+    
+    console.log("añadido a favorito");
+
+}
+
+function favoritos() {
+    var datos = '';
+    console.log('estoy en favoritos');
+    var favoritos = JSON.parse(localStorage.getItem('mascotas'));
+    console.log(favoritos);
+
+    const row = document.createElement('div');
+    row.innerHTML =`
+    <div class="row container categoria-mascotas">
+        <div class="col-5 Adopta">
+            <div class="contenedorMascotas">
+                <img src="${favoritos.img}" id="${favoritos.nombre}" class="mascotaFav" alt="">
+                <h3 id="nombreMascota" >${favoritos.nombre}</h3>
+                <p id="razaMascota">${favoritos.raza}</p>
+            </div>
+        </div>
+    </div>
+    `
+    console.log(row);
+
+    const espacioFavoritos = document.querySelector('.container');
+
+    espacioFavoritos.appendChild(row);
+
+}
+
+
